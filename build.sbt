aad414c801a100d841b12b5334825699bbeb9c56
felipexw <felipexw@gmail.com>
2016-08-16 13:16:40 -0300

    Changed the item attributes for domain purposes

--- build.sbt
+++ build.sbt
@@ -7,7 +7,7 @@ name := "template-scala-parallel-svd-item-similarity"
 organization := "io.prediction"
 
 libraryDependencies ++= Seq(
-  "io.prediction"    %% "core"          % pioVersion.value % "provided",
+  "io.prediction"    %% "core"          % "0.9.5" % "provided",
   "org.apache.spark" %% "spark-core"    % "1.3.0" % "provided",
   "org.apache.spark" %% "spark-mllib"   % "1.3.0" % "provided",
   "org.xerial.snappy" % "snappy-java" % "1.1.1.7")
--- data/import_eventserver.py
+++ data/import_eventserver.py
@@ -11,13 +11,14 @@ import re
 SEED = 12
 NUMSAMPLES = 1000
 
+
 def prepare_data():
     random.seed(SEED)
     res = []
     with open("./sample_actors.txt") as f:
         actors = [actor for actor in f.read().split("\n") if actor != ""]
     f.close()
-    
+
     with open("./sample_producers.txt") as f:
         producers = [producer for producer in f.read().split("\n")
                      if producer != ""]
@@ -27,35 +28,30 @@ def prepare_data():
         directors = [director for director in f.read().split("\n")
                      if director != ""]
     f.close()
-    
+
     with open("./sample_movies.csv", "rb") as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
             data = {}
             title = row[1]
-            genres = row[2].split("|")
-            data["genres"] = genres
-            data["title"] = title
+            genres = row[2].split("|")[0]
+            data["unidadeDeMedida"] = genres
+            data["linhaDeProdutoDescricao"] = title
             data["id"] = row[0]
-            
-            match_year = re.search(".+(\d\d\d\d)", title)
-            if match_year:
-                year = match_year.group(1)
-                data["year"] = int(year)
-                # Now magic starts
-                data["producer"] = random.sample(producers, 1).pop()
-                data["director"] = random.sample(directors, 1).pop()
-                data["duration"] = random.randrange(72, 180, 1)
-                num_actors = random.randrange(7, 25, 1)
-                data["actors"] = random.sample(actors, num_actors)
-                res.append(data)
+
+            data["familiaDescricao"] = random.sample(producers, 1).pop()
+            data["empresaDescricao"] = random.sample(directors, 1).pop()
+            data["descricao"] = random.randrange(72, 180, 1)
+            num_actors = random.randrange(7, 25, 1)
+            data["actors"] = random.sample(actors, num_actors)
+            res.append(data)
     f.close()
     return res
-                
+
 
 def import_events(client, data):
     count = 0
-    
+
     for el in data[0:NUMSAMPLES]:
         count += 1
         print("%d / %d" % (count, NUMSAMPLES))
@@ -66,23 +62,23 @@ def import_events(client, data):
             properties=el)
 
     print("%s events are imported." % count)
-            
-        
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Import sample data for similar items by attributes engine")
     parser.add_argument('--access_key', default='invald_access_key')
     parser.add_argument('--url', default="http://localhost:7070")
-    
+
     args = parser.parse_args()
     print(args)
-    
-    client = predictionio.EventClient(access_key=args.access_key, url=args.url, 
-        threads=4, qsize=100)
-    
+
+    client = predictionio.EventClient(access_key=args.access_key, url=args.url,
+                                      threads=4, qsize=100)
+
     data = prepare_data()
     import_events(client, data)
-    
-    
+
+
 if __name__ == '__main__':
     main()
